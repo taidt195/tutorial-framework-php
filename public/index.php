e8d1dfaab8fa8c3968e3bfcdbb225f664fcf9fa4
TaiDT <tai.programer@gmail.com>
2017-06-01 09:35:50 +0700

    Test deploy

--- public/index.php
+++ public/index.php
@@ -1,4 +1,8 @@
 <?php
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
+
 	require_once(dirname(__DIR__).'/app/core/App.php');
 	$config = require_once(dirname(__DIR__).'/config/main.php');
 
